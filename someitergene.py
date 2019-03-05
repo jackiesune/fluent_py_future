4cfb1339b3418e6a6c4481f22da793a99c87b769
jack.s <my_email@exp.co>
2019-03-05 21:58:19 +0800

    use itertools for class Arithtmetic

--- someitergene.py
+++ someitergene.py
@@ -4,8 +4,8 @@ class Am:
         self.end=end
         self.step=step
 
-    def __iter__():
-        result=type(self.begin+self.end)(self.begin)
+    def __iter__(self):
+        result=type(self.begin+self.step)(self.begin)
         index=0
         forever=self.end is None
         while forever or result<self.end:
@@ -13,3 +13,10 @@ class Am:
             index+=1
             result=result+self.step*index
 
+import itertools
+def gen_am(start,step,end=None):
+    result=type(start+step)(start)
+    ap_gen=itertools.count(result,step)
+    if end is not None:
+        ap_gen=itertools.takewhile(lambda n:n<end,ap_gen)
+    return ap_gen
